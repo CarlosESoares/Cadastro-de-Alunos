2875d3cbec3782846ae376013bf71ea1e40671ff
JoaoR <joaobataglion087@gmail.com>
2026-04-23 23:31:06 -0300

    novas configurações de interação com o servidor

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require 'sinatra'
 require 'mongo'
 require 'bcrypt'
+require 'json'
 
 set :bind, '0.0.0.0'
 set :port, 4567
@@ -31,7 +32,6 @@ helpers do
                        :auth_source => 'projeto')
     end
   end
-
   def registrar_log(tipo_acao, info_extra = "")
     begin
       db[:log].insert_one({
@@ -49,26 +49,39 @@ helpers do
   end
 end
 
-# Rota Principal
+def db = Mongo::Client.new(['100.70.14.58:27017'], 
+                       :database => 'projeto', 
+                       :user => 'app_user', 
+                       :password => 'Projeto-APP-acesso', 
+                       :auth_source => 'projeto')
+
 get '/' do
-  begin
-    @itens = db[:alunos].find.to_a
-    
-    if @itens.empty?
-      puts "Aviso: Conectou, mas a coleção 'alunos' está vazia."
-    end
-    
-  rescue => e
-    puts "ERRO DE CONEXÃO: #{e.message}"
-    @itens = [{nome: "Erro: #{e.message}"}]
-  end
   erb :index
+end
+
+# Rota para obter a lista de alunos em formato JSON
+get '/alunos' do
+  content_type :json
+  begin
+    alunos = db[:alunos].find.to_a.map do |doc| 
+  { 
+    id: doc[:_id].to_s, 
+    nome: doc[:nome],
+    matricula: doc[:matricula], 
+    turma: doc[:turma],
+    disciplinas: doc[:disciplinas] || [],
+  } 
+end
+    alunos.to_json
+  rescue => e
+    puts "Erro ao buscar alunos: #{e.message}"
+    [].to_json
+  end
 end
 
 # Rota do Login
 post '/login' do
   user_doc = db[:usuarios].find(username: params[:username]).first
-
   if user_doc && BCrypt::Password.new(user_doc[:password_hash]) == params[:password]
     session[:admin] = true
     registrar_log("LOGIN_SUCESSO", "Usuário #{params[:username]} logou")
@@ -79,40 +92,35 @@ post '/login' do
     redirect '/?login_error=1'
   end
 end
-
-get '/logout' do
-  # Registra que o admin está saindo antes de apagar a sessão
-  if session[:admin]
-    registrar_log("LOGOUT", "O administrador encerrou a sessão")
-  end
-
-  session.clear
-  redirect '/'
-end
-
-post '/inserir' do
-  if session[:admin]
-    result = db[:alunos].insert_one({ nome: params[:nome] })
-    # Registra a ação
-    registrar_log("INSERCAO", "Inseriu o aluno com id: #{result.inserted_id}")
-    redirect '/'
-  else
-    # registra tentativa falha
-    registrar_log("TENTATIVA_NEGADA", "Visitante tentou inserir aluno")
-    halt 403, "Acesso negado"
-  end
-end
-
-# Rota para deletar um aluno
-get '/deletar/:id' do
-  if session[:admin]
-    # Registra antes de deletar para ter o ID no log
-    registrar_log("EXCLUSAO", "Deletou o aluno com ID: #{params[:id]}")
-    
-    id_bson = BSON::ObjectId.from_string(params[:id])
-    db[:alunos].delete_one({ _id: id_bson })
-  else
-    registrar_log("TENTATIVA_NEGADA", "Visitante tentou deletar ID: #{params[:id]}")
-  end
-  redirect '/'
-end
+# get '/logout' do
+#   # Registra que o admin está saindo antes de apagar a sessão
+#   if session[:admin]
+#     registrar_log("LOGOUT", "O administrador encerrou a sessão")
+#   end
+#   session.clear
+#   redirect '/'
+# end
+# post '/inserir' do
+#   if session[:admin]
+#     result = db[:alunos].insert_one({ nome: params[:nome] })
+#     # Registra a ação
+#     registrar_log("INSERCAO", "Inseriu o aluno com id: #{result.inserted_id}")
+#     redirect '/'
+#   else
+#     # registra tentativa falha
+#     registrar_log("TENTATIVA_NEGADA", "Visitante tentou inserir aluno")
+#     halt 403, "Acesso negado"
+#   end
+# end
+# # Rota para deletar um aluno
+# get '/deletar/:id' do
+#   if session[:admin]
+#     # Registra antes de deletar para ter o ID no log
+#     registrar_log("EXCLUSAO", "Deletou o aluno com ID: #{params[:id]}")
+#     id_bson = BSON::ObjectId.from_string(params[:id])
+#     db[:alunos].delete_one({ _id: id_bson })
+#   else
+#     registrar_log("TENTATIVA_NEGADA", "Visitante tentou deletar ID: #{params[:id]}")
+#   end
+#   redirect '/'
+# end
